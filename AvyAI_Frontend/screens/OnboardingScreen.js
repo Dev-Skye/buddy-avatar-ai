32c9af99b27f29c82a1070355c7727967f0e6e0a
Dev-Skye <joyskye09@gmail.com>
2025-10-13 08:10:20 +0100

    edited onboarding screen colour

--- AvyAI_Frontend/screens/OnboardingScreen.js
+++ AvyAI_Frontend/screens/OnboardingScreen.js
@@ -45,7 +45,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        //backgroundColor: '#300364cf',
         backgroundColor: '#070865ff'
     },
     baseText:{
